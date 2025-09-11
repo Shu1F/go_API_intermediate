54c870898a0859efd739d86b8d5f1c3a646b008c
Shuichi <fs2031@icloud.com>
2025-09-11 16:11:37 +0900

    reset

--- db/insertData.sql
+++
@@ -1,9 +0,0 @@
-insert into articles (title, contents, username, nice, created_at) values
-('first Post', 'This is my first blog', 'Shuichi', 2, now());
-insert into articles (title, contents, username, nice) values
-('2nd', 'Second post', 'Shuichi', 4);
-
-insert into comments (article_id, message, created_at) values
-(1, 'Great post!', now());
-insert into comments (article_id, message) values
-(1, 'Thanks for sharing');
